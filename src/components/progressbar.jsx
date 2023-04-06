7a48076f7a5ff0f9a443739a01fe79e148702adf
Abhishek Epili <epiliabhishek@gmail.com>
2023-04-06 12:52:51 +0530

    fourth

--- src/components/progressbar.jsx
+++ src/components/progressbar.jsx
@@ -1,7 +1,7 @@
 function ProgressBar(value) {
     return (
         <>
-            <div className="progress" style={{height: "20px", width:"75%"}}>
+            <div className="progress" style={{height: "4vh", width:"75%"}}>
                 <div className="progress-bar bg-success" role="progressbar" style={value.progressbarstyle} aria-valuemin="0" aria-valuemax="100">{value.progress}</div>
             </div>
         </>
--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -2,39 +2,50 @@ import ProgressBar from "./progressbar"
 function Skills() {
     return (
         <>
-            <div className="skills-content">
-                <center><h2>My Skills</h2></center><br/>
-                <img src="./images/html.png" height="40"/><img src="./images/css.png" height="40"/>&nbsp;&nbsp; HTML,CSS
+            <div className="skills-container">
+                <center><h2>My Skills</h2></center>
+                <div >
+                <img src="./images/html.png" height="40"/><img src="./images/css.png" height="40"/>&nbsp;&nbsp; HTML,CSS<br/>
                 <ProgressBar
                     progressbarstyle={{ width: "90%" }}
                     progress="90%"
                 />
-                <img src="./images/php.png" height="40" style={{borderRadius: "50%"}}/>&nbsp;&nbsp; PHP
+                </div>
+                <div>
+                <img src="./images/php.png" height="40" style={{borderRadius: "50%"}}/>&nbsp;&nbsp; PHP<br/>
                 <ProgressBar
                     progressbarstyle={{ width: "65%" }}
                     progress="65%"
                 />
-                <img src="./images/mysql.png" height="50"/>&nbsp;&nbsp; MYSQL
+                </div>
+                <div >
+                <img src="./images/mysql.png" height="50"/>&nbsp;&nbsp; MYSQL<br/>
                 <ProgressBar
                     progressbarstyle={{ width: "85%" }}
                     progress="85%"
                 />
-                <img src="./images/java.png" height="40"/>&nbsp;&nbsp; Java
+                </div>
+                <div >
+                <img src="./images/java.png" height="40"/>&nbsp;&nbsp; Java<br/>
                 <ProgressBar
                     progressbarstyle={{ width: "78%" }}
                     progress="78%"
                 />
-                
-                <img src="./images/react.png" height="40"/>&nbsp;&nbsp; React JS
+                </div>
+                <div >
+                <img src="./images/react.png" height="40"/>&nbsp;&nbsp; React JS<br/>
                 <ProgressBar
                     progressbarstyle={{ width: "60%" }}
                     progress="60%"
                 />
-                <img src="./images/python.png" height="40"/>&nbsp;&nbsp; Python
+                </div>
+                <div >
+                <img src="./images/python.png" height="40"/>&nbsp;&nbsp; Python<br/>
                 <ProgressBar
                     progressbarstyle={{ width: "80%" }}
                     progress="80%"
                 />
+                </div>
             </div>
         </>
     )
