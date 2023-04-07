d3ef2efd64a0b7b23d612233c41d77ada70c0f3a
Abhishek Epili <epiliabhishek@gmail.com>
2023-04-07 15:31:42 +0530

    updating ui

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -5,48 +5,66 @@ function Skills() {
             <div className="skills-container">
                 <center><h2>My Skills</h2></center>
                 <div >
-                <img src="/images/html.png" height="40"/><img src="/images/css.png" height="40"/>&nbsp;&nbsp; HTML,CSS<br/>
-                <ProgressBar
-                    progressbarstyle={{ width: "90%" }}
-                    progress="90%"
-                />
+                    <div className="skills-text">
+                        <img src="/images/html.png" height="40" /><img src="/images/css.png" height="40" />&nbsp;&nbsp; HTML,CSS
+                    </div>
+                    
+                    <ProgressBar
+                        progressbarstyle={{ width: "90%" }}
+                        progress="90%"
+                    />
                 </div>
                 <div>
-                <img src="/images/php.png" height="40" style={{borderRadius: "50%"}}/>&nbsp;&nbsp; PHP<br/>
-                <ProgressBar
-                    progressbarstyle={{ width: "65%" }}
-                    progress="65%"
-                />
+                    <div className="skills-text">
+                        <img src="/images/php.png" height="40" style={{ borderRadius: "50%" }} />&nbsp;&nbsp; PHP
+                    </div>
+                    
+                    <ProgressBar
+                        progressbarstyle={{ width: "65%" }}
+                        progress="65%"
+                    />
                 </div>
                 <div >
-                <img src="/images/mysql.png" height="50"/>&nbsp;&nbsp; MYSQL<br/>
-                <ProgressBar
-                    progressbarstyle={{ width: "85%" }}
-                    progress="85%"
-                />
+                    <div className="skills-text">
+                        <img src="/images/mysql.png" height="50" />&nbsp;&nbsp; MYSQL
+                    </div>
+                    
+                    <ProgressBar
+                        progressbarstyle={{ width: "85%" }}
+                        progress="85%"
+                    />
                 </div>
                 <div >
-                <img src="/images/java.png" height="40"/>&nbsp;&nbsp; Java<br/>
-                <ProgressBar
-                    progressbarstyle={{ width: "78%" }}
-                    progress="78%"
-                />
+                    <div className="skills-text">
+                        <img src="/images/java.png" height="40" />&nbsp;&nbsp; Java
+                    </div>
+                    
+                    <ProgressBar
+                        progressbarstyle={{ width: "78%" }}
+                        progress="78%"
+                    />
                 </div>
                 <div >
-                <img src="/images/react.png" height="40"/>&nbsp;&nbsp; React JS<br/>
-                <ProgressBar
-                    progressbarstyle={{ width: "60%" }}
-                    progress="60%"
-                />
+                    <div className="skills-text">
+                        <img src="/images/react.png" height="40" />&nbsp;&nbsp; React JS
+                    </div>
+                    
+                    <ProgressBar
+                        progressbarstyle={{ width: "60%" }}
+                        progress="60%"
+                    />
                 </div>
                 <div >
-                <img src="/images/python.png" height="40"/>&nbsp;&nbsp; Python<br/>
-                <ProgressBar
-                    progressbarstyle={{ width: "80%" }}
-                    progress="80%"
-                />
+                    <div className="skills-text">
+                        <img src="/images/python.png" height="40" />&nbsp;&nbsp; Python
+                    </div>
+                    
+                    <ProgressBar
+                        progressbarstyle={{ width: "80%" }}
+                        progress="80%"
+                    />
                 </div>
-            </div>
+            </div >
         </>
     )
 }
